5ea828e98eabdb18157eae2f7fef882a08d7ae60
han xu <hanxu27@gmail.com>
2019-03-01 03:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, name) {
   line.push(name)
   var i = line.length
-  return `Welcome, ${name}. You are number ${} in line.`
+  return `Welcome, ${name}. You are number ${i} in line.`
 }
