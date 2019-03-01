98391310820fdb40183cfb188768b731b400e627
han xu <hanxu27@gmail.com>
2019-03-01 03:30:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(line, name) {
   var i = line.length
   return `Welcome, ${name}. You are number ${i} in line.`
 }
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return ("there is nobody waiting to be served")
+  } else {
+    
+  }
+  
+}
