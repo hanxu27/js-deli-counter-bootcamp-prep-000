a52dc23148006257cc8b8f79f63704954b0e2c9e
han xu <hanxu27@gmail.com>
2019-03-01 04:16:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(line) {
 
 function currentLine(line) {
   if (line.length === 0) {
-    return ("The line is currently empty")
+    return ("The line is currently empty.")
   } else {
     while (i++ < line.length) {
       output =+ [i + 1] + '. ' + line[i]
