5f1242013c891b12530765d12db79c42d32a7d61
han xu <hanxu27@gmail.com>
2019-03-01 04:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(line) {
   if (line.length === 0) {
     return ("There is nobody waiting to be served!")
   } else {
-    console.return(`Currently serving ${line[0]}.`)
+    console.log(`Currently serving ${line[0]}.`)
     return (line.unShift)
   }
 }
