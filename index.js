e6cb34381417fc07cd2a55ea58164c018582e90a
han xu <hanxu27@gmail.com>
2019-03-01 04:43:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ function currentLine(line) {
   } else {
     var i = 0
     var output = []
-    while (i++ < line.length) {
+    while (i < line.length) {
       output[i] = [i + 1] + '. ' + line[i]
+      i++
     }
   }
   return (`The line is currently: ${output}`)
